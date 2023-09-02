4ba8f8aa75b9576c7342e46476cc361e75247f18
Ba3yoo <115632689+Ba3yoo@users.noreply.github.com>
2023-09-02 14:14:34 +0700

    Flow of The game

--- There is a Murder in The Train/Assets/Script/Manager/EndingManager.cs
+++ There is a Murder in The Train/Assets/Script/Manager/EndingManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndingManager : MonoBehaviour
 {
     public static EndingManager instance;
     [SerializeField] private GameObject fadeBlack;
+    [SerializeField] private GameObject EndingCanvas;
 
     public void Awake()
     {
         instance = this;
+        EndingCanvas.gameObject.SetActive(false);
     }
     public IEnumerator FadeTo(float aValue, float aTime)
     {
@@ -20,5 +23,13 @@ public class EndingManager : MonoBehaviour
             fadeBlack.GetComponent<SpriteRenderer>().color = newColor;
             yield return null;
         }
+        ClueManager.instance.ClueButton.gameObject.SetActive(false);
+        StartCoroutine(LoadScene());
+    }
+    public IEnumerator LoadScene()
+    {
+        EndingCanvas.gameObject.SetActive(true);
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("MainMenu");
     }
 }
